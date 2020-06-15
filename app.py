827d8da7134dc136f8ff0aa410f5e7a755aac2a3
Pushpak741 <pushpakganagoni@gmail.com>
2020-06-15 12:17:14 +0530

    felina changes

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_restful import Api
-from resources.People import Ppl
+from flask_jwt_extended import  JWTManager
+from resources.People import Ppl,PplLogin
 
 app=Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS']=True
+app.config['JWT_SECRET_KEY']='coscskillup'
 api=Api(app)
-
+jwt=JWTManager(app)
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+            'error':'authorized_required',
+            "description":"Request does not contain an access token."
+    }),401
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'error':'invalid_token',
+        'message':'Signature verification failed.'
+    }),401
 api.add_resource(Ppl,'/people')
+api.add_resource(PplLogin,'/login')
 if __name__=='__main__':
     app.run()
--- resources/People.py
+++ resources/People.py
@@ -1,7 +1,10 @@
 from flask_restful import Resource,reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token,jwt_required
 from db import query
 
 class Ppl(Resource):
+    @jwt_required
     def get(self):
         parser=reqparse.RequestParser()
         parser.add_argument('pId',type=int,required=True,help="pId cannot be left blank")
@@ -10,6 +13,7 @@ class Ppl(Resource):
             return query(f"""select * from restapi.People where pId={data['pId']};""")
         except:
             return {"message":"THERE WAS AN ERROR CONNECTING TO PEOPLE TABLE"},500
+    @jwt_required
     def post(self):
         parser=reqparse.RequestParser()
         parser.add_argument('pId',type=str,required=True,help="pId cannot be left empty")
@@ -30,3 +34,33 @@ class Ppl(Resource):
             return{"message":"Successfully inserted"},201
         except:
             return {"message":"THERE WAS AN ERROR INSERTING INTO PEOPLE"},500
+
+class User():
+    def __init__(self,pId,pname,password):
+        self.pId=pId
+        self.pname=pname
+        self.password=password
+
+    @classmethod
+    def getUserBypId(cls,pId):
+        result=query(f"""select pId,pname,pass from restapi.People where pId='{pId}'""",return_json=False)
+        if len(result)>0: return User(result[0]['pId'],result[0]['pname'],result[0]['pass'])
+        return none
+
+    @classmethod
+    def getUserBypname(cls,pname):
+        result=query(f"""select pId,pname,pass from restapi.People where pname='{pname}'""",return_json=False)
+        if len(result)>0: return User(result[0]['pId'],result[0]['pname'],result[0]['pass'])
+        return none
+
+class PplLogin(Resource):
+    def post(self):
+        parser=reqparse.RequestParser()
+        parser.add_argument('pname',type=str,required=True,help="pId cannot be left blank")
+        parser.add_argument('pass',type=str,required=True,help="pass cannot be left blank")
+        data=parser.parse_args()
+        user=User.getUserBypname(data['pname'])
+        if user and safe_str_cmp(user.password,data['pass']):
+            access_token=create_access_token(identity=user.pId,expires_delta=False)
+            return{'access_token':access_token},200
+        return{"message":"Invalid Credentials"},401
